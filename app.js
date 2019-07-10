7b2fb08bd9ee39bdc3ac49796680626d2f4f642f
Vesper Jones <vesperjones@Vespers-MacBook-Pro.local>
2019-07-09 23:13:03 -0500

    added the first event listener

--- app.js
+++ app.js
@@ -1,36 +1,49 @@
+
+//BUDGET CONTROLLER
 var budgetController = (function() {
 
-    var x = 23;
-    var add = function(a) {
-        return x + a 
-    }
 
-    return{
-        //publicTest is what we call a "method"
-        //it can access the variables above though they are out of the scope
-        publicTest: function(b) {
-            return add(b);
-
-        }
-    }
 
 })();
 
 
-
+//UI CONTROLLER
 var UIController = (function() {
 
 })();
 
 
+
+//GLOBAL APP CONTROLLER
+
 var controller = (function(bundgetCtrl, UICtrl) {
 
-    var z = bundgetCtrl.publicTest(5);
+    //dry - dont repeat yourself - so we are creting a function to re-use later
+    var ctrlAddItem = function() {
 
-    return{
-        anotherPublic:function(){
-            console.log(z);
-        }
+        //1 Get the field input data
+
+      //2 add the item to the budget controller
+
+      //3 add the new item to the new UI 
+
+      //4 calculate the budget
+
+      //5 display the budget on the UI
+
+        console.log('It works bitches');
     }
 
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+  //this will allow users to add the input by pressing 'enter'
+  //note we are using even.which (for older browsers sane thing as event.keycode)
+  document.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+    }
+  });
+
+
+    
 })(budgetController, UIController);
