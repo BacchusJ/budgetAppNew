d23dbba3cde53341546eb74cb34dc54f776c93a5
Vesper Jones <vesperjones@Vespers-MacBook-Pro.local>
2019-07-12 20:56:56 -0500

    adding functions to update the UI

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ var budgetController = (function() {
 //UI CONTROLLER
 var UIController = (function() {
 
+    return {
+        getinput: function () {
+            return {
+
+                type: document.querySelector('.add__type').value, //will be either inc or exp
+                description:  document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value,
+
+            };
+        }
+    };
+
 })();
 
 
@@ -23,6 +35,10 @@ var controller = (function(bundgetCtrl, UICtrl) {
 
         //1 Get the field input data
 
+        var input = UICtrl.getInput
+
+        console.log(input);
+
       //2 add the item to the budget controller
 
       //3 add the new item to the new UI 
@@ -31,7 +47,6 @@ var controller = (function(bundgetCtrl, UICtrl) {
 
       //5 display the budget on the UI
 
-        console.log('It works');
     }
 
   document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
@@ -41,7 +56,9 @@ var controller = (function(bundgetCtrl, UICtrl) {
   document.addEventListener('keypress', function(event) {
     if (event.keyCode === 13 || event.which === 13) {
         ctrlAddItem();
-    }
+        }
+
+
   });
 
 
