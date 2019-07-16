868aa3ed6533f12606cb9f516a5a5c9d16fe692b
Bacchus <47571329+BacchusJ@users.noreply.github.com>
2019-07-16 18:03:56 -0500

    created new items
    push those items to the structure
    return the new items 
    getting the field input data and adding the item to the budget controller =)

--- app.js
+++ app.js
@@ -24,7 +24,36 @@ var budgetController = (function() {
             exp: 0,
             inc: 0
         }
-    }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+            //create new ID 
+            if (data.allItems[type].length > 0) {
+                ID  = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+
+            }
+            
+
+            //create new item based on inc exp type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            //push it to the tructure
+            data.allItems[type].push(newItem);
+            //return new item 
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    };
 
 })();
 
@@ -81,10 +110,11 @@ var controller = (function(budgetCtrl, UICtrl) {
     
     //dry - dont repeat yourself - so we are creting a function to re-use later
     var ctrlAddItem = function() {
-
+        var input, newItem;
         //1 Get the field input data
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
       //2 add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
       //3 add the new item to the new UI 
 
