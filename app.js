2a1b024826bcb19d54efb855e86377a019025ef6
Bacchus <47571329+BacchusJ@users.noreply.github.com>
2019-07-29 21:38:59 -0500

    updating the DOM
    querySeclector on the new created object

--- app.js
+++ app.js
@@ -168,7 +168,10 @@ var UIController = (function() {
 
         displaybudget: function(obj) {
 
-
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
 
 
         },
